45ffb0a4a08ff8c944d171d72f5018ce2614626e
LEPSVISION <LEPSVIDZE@GMAIL.COM>
2022-07-28 13:45:35 +0300

    add localstorage

--- main.js
+++ main.js
@@ -1,69 +1,68 @@
+const TaskComponent = (value) => {
+    return (`
+      <div class="task" attr-value="${value}">
+        <div class="content">
+            <input type="text" class="text" value="${value}" readonly>
+        </div>
+        <div class="actions">
+            <button class="edit">Edit</button>
+            <button class="delete">Delete</button>
+        </div>
+      </div>
+    `)
+};
+
+
 window.addEventListener('load', () => {
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
-    const list_el = document.querySelector("#tasks");
+    const tasksList = document.querySelector("#tasks");
 
-    let todos = JSON.parse(localStorage.getItem('todos') || "[]");
+    const todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+    const updateTodosStorage = () => localStorage.setItem('todos', JSON.stringify(todos));
+
+    tasksList.innerHTML = todos.map(TaskComponent).join('');
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
+        const value = input.value.trim();
 
-        const task = input.value;
-
-        if(task) {
-            todos.push(task);
-            localStorage.setItem('todos', JSON.stringify(todos));   
+        if (value && !todos.includes(value)) {
+            todos.push(value);
+            updateTodosStorage();
+            tasksList.innerHTML += TaskComponent(value);
         }
 
-        const task_el = document.createElement("div");
-        task_el.classList.add("task");
-
-        const task_content_el = document.createElement("div");
-        task_content_el.classList.add("content");
-
-        task_el.appendChild(task_content_el);
-
-        const task_input_el = document.createElement("input");
-        task_input_el.classList.add("text");
-        task_input_el.type = "text";
-        task_input_el.value = task;
-        task_input_el.setAttribute("readonly", "readonly");
-
-        task_content_el.appendChild(task_input_el);
-
-        const task_actions_el = document.createElement("div");
-        task_actions_el.classList.add("actions");
-
-        const task_edit_el = document.createElement("button");
-        task_edit_el.classList.add("edit");
-        task_edit_el.innerHTML = "Edit";
-
-        const task_delete_el = document.createElement("button");
-        task_delete_el.classList.add("delete");
-        task_delete_el.innerHTML = "Delete";
-
-        task_actions_el.appendChild(task_edit_el);
-        task_actions_el.appendChild(task_delete_el);
-
-        task_el.appendChild(task_actions_el);
-
-        list_el.appendChild(task_el);
-
         input.value = "";
+    });
 
-        task_edit_el.addEventListener('click', () => {
-            if(task_edit_el.innerText.toLowerCase() == "edit"){
-                task_input_el.removeAttribute("readonly");
-                task_input_el.focus();
-                task_edit_el.innerText = "Save";
-            }else{
-                task_input_el.setAttribute("readonly", "readonly");
-                task_edit_el.innerText = "Edit";
-            }            
-        });
+    tasksList.addEventListener('click', (e) => {
+        const element = e.target;
+        const selectedTask = element.closest('.task');
+        const inputElement = selectedTask.querySelector('.text');
 
-        task_delete_el.addEventListener('click', () => {
-            list_el.removeChild(task_el)
-        })
-    })
+        const selectedTaskValue = selectedTask.getAttribute('attr-value');
+        const selectedTaskIndex = todos.findIndex(value => value === selectedTaskValue);
+
+        const isEditButton = element.classList.contains('edit');
+        const isDeleteButton = element.classList.contains('delete');
+
+        if (isEditButton) {
+            if (inputElement.readOnly) {
+                inputElement.readOnly = false;
+                inputElement.focus();
+                element.innerText = "Save";
+            } else {
+                todos[selectedTaskIndex] = inputElement.value;
+                updateTodosStorage();
+                inputElement.readOnly = true;
+                element.innerText = "Edit";
+            }
+        } else if (isDeleteButton) {
+            todos.splice(selectedTaskIndex, 1);
+            updateTodosStorage();
+            tasksList.removeChild(selectedTask);
+        }
+    });
 })
